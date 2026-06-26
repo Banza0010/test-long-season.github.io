1c9bf52706f0fe6561b188c76a61e2b2d23da2b2
Banza Inc <banzainc@Banzas-MacBook-Pro.local>
2026-06-26 19:20:36 +0200

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,20 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
-    resolveId(id) {
+    resolveId(id: string) {
       if (id.startsWith('figma:asset/')) {
         const filename = id.replace('figma:asset/', '')
-        return path.resolve(__dirname, 'src/assets', filename)
+        return path.resolve('src/assets', filename)
       }
     },
   }
 }
 
 export default defineConfig({
+  base: '/test-long-season.github.io/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
@@ -27,7 +27,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve('src'),
     },
   },
 
